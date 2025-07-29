e49e611a30c80b1794129938e7b38cb740da7094
Phillip Schwartz <philliptschwartz@gmail.com>
2025-07-29 22:36:56 +0300

    Clone initial backend setup

--- services/bug.service.js
+++ services/bug.service.js
@@ -1,63 +1,78 @@
-const BASE_URL = 'http://127.0.0.1:3030/api/bug'
+import { utilService } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const STORAGE_KEY = 'bugs'
+
+_createBugs()
 
 export const bugService = {
     query,
     getById,
     save,
     remove,
-    getDefaultFilter,
+    getDefaultFilter
 }
 
 function query(filterBy) {
-    return fetch(BASE_URL, {
-        method: 'GET',
-        credentials: 'include', // for using cookies
+    return storageService.query(STORAGE_KEY)
+    .then(bugs => {
+
+        if (filterBy.txt) {
+            const regExp = new RegExp(filterBy.txt, 'i')
+            bugs = bugs.filter(bug => regExp.test(bug.title))
+        }
+
+        if (filterBy.minSeverity) {
+            bugs = bugs.filter(bug => bug.severity >= filterBy.minSeverity)
+        }
+
+        return bugs
     })
-        .then(res => res.json())
-        .then(bugs => {
-            if (!filterBy) return bugs
-
-            let filteredBugs = bugs
-
-            if (filterBy.txt) {
-                const regExp = new RegExp(filterBy.txt, 'i')
-                filteredBugs = filteredBugs.filter(bug => regExp.test(bug.title) || regExp.test(bug.description))
-            }
-
-            if (filterBy.minSeverity) {
-                filteredBugs = filteredBugs.filter(bug => bug.severity >= filterBy.minSeverity)
-            }
-
-            return filteredBugs
-        })
 }
 
 function getById(bugId) {
-    return fetch(`${BASE_URL}/${bugId}`, {
-        method: 'GET',
-        credentials: 'include', // cookies
-    }).then(res => res.json())
+    return storageService.get(STORAGE_KEY, bugId)
 }
 
 function remove(bugId) {
-    return fetch(`${BASE_URL}/${bugId}/remove`, {
-        method: 'GET',
-        credentials: 'include', // cookies
-    }).then(res => res.json())
+    return storageService.remove(STORAGE_KEY, bugId)
 }
 
 function save(bug) {
-    const queryParams = new URLSearchParams()
+    if (bug._id) {
+        return storageService.put(STORAGE_KEY, bug)
+    } else {
+        return storageService.post(STORAGE_KEY, bug)
+    }
+}
 
-    if (bug._id) queryParams.append('_id', bug._id)
-    queryParams.append('title', bug.title)
-    queryParams.append('severity', bug.severity)
-    queryParams.append('description', bug.description || '')
+function _createBugs() {
+    let bugs = utilService.loadFromStorage(STORAGE_KEY)
+    if (bugs && bugs.length > 0) return 
 
-    return fetch(`${BASE_URL}/save?${queryParams.toString()}`, {
-        method: 'GET',
-        credentials: 'include', // cookies
-    }).then(res => res.json())
+    bugs = [
+        {
+            title: "Infinite Loop Detected",
+            severity: 4,
+            _id: "1NF1N1T3"
+        },
+        {
+            title: "Keyboard Not Found",
+            severity: 3,
+            _id: "K3YB0RD"
+        },
+        {
+            title: "404 Coffee Not Found",
+            severity: 2,
+            _id: "C0FF33"
+        },
+        {
+            title: "Unexpected Response",
+            severity: 1,
+            _id: "G0053"
+        }
+    ]
+    utilService.saveToStorage(STORAGE_KEY, bugs)
 }
 
 function getDefaultFilter() {
